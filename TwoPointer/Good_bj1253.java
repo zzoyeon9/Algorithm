f6b1b9c22b08007b3ec918c7b9a44163cfe61054
zzoyeon9 <zzoyeon9@naver.com>
2022-08-11 10:25:16 +0900

    refactoring filename

--- TwoPointer/Good_bj1253.java
+++ TwoPointer/Good_bj1253.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Good_bj1253 {
 
     private static int N;
     private static int[] arr;
