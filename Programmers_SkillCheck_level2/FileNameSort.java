b8c697662032aaa83a4a8b0cff03c2b191086c45
zzoyeon9 <zzoyeon9@naver.com>
2022-10-14 12:12:53 +0900

    Challenge Programmers SkillCheck level2

---
+++ Programmers_SkillCheck_level2/FileNameSort.java
@@ -0,0 +1,46 @@
+package Practice;
+
+import java.util.*;
+
+public class FileNameSort {
+
+    public String[] solution(String[] files) {
+
+        Arrays.sort(files, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                String str1 = o1.toLowerCase(Locale.ROOT);
+                String str2 = o2.toLowerCase(Locale.ROOT);
+
+                String head1 = str1.split("[0-9]")[0];
+                String head2 = str2.split("[0-9]")[0];
+
+                if(!head1.equals(head2))
+                    return head1.compareTo(head2);
+
+                String num1 = str1.substring(head1.length()).split("[^0-9]")[0];
+                String num2 = str2.substring(head2.length()).split("[^0-9]")[0];
+
+                while(num1.split("")[0].equals("0") && num1.length()>1)
+                    num1 = num1.substring(1);
+
+                while(num2.split("")[0].equals("0") && num2.length()>1)
+                    num2 = num2.substring(1);
+
+                if(!num1.equals(num2))
+                    return Integer.parseInt(num1) - Integer.parseInt(num2);
+
+                return 0;
+            }
+        });
+        String[] answer = files;
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FileNameSort s = new FileNameSort();
+        String [] arr = {"A0","AbC12","a1"};
+        System.out.print(Arrays.toString(s.solution(arr)));
+    }
+}
---
+++ Programmers_SkillCheck_level2/TargetNumber.java
@@ -0,0 +1,41 @@
+package Practice.src;
+
+public class Solution {
+
+    int count = 0;
+
+    public int solution(int[] numbers, int target) {
+        int seq = 0;
+        int sum = 0;
+
+        dfs(seq + 1, sum + numbers[0], target, numbers);
+        dfs(seq + 1, sum - numbers[0], target, numbers);
+
+        int answer = count;
+        return answer;
+    }
+
+    private void dfs(int seq, int sum, int target, int[] numbers) {
+
+        if (seq == numbers.length) {
+            if (sum == target)
+                count++;
+            return;
+        }
+        dfs(seq + 1, sum + numbers[seq], target, numbers);
+        dfs(seq + 1, sum - numbers[seq], target, numbers);
+    }
+
+
+    public static void main(String[] args) {
+        int[] nums1 = {1, 1, 1, 1, 1};
+        int tar1 = 3;
+        int[] nums2 = {4, 1, 2, 1};
+        int tar2 = 4;
+
+        Solution s = new Solution();
+        System.out.println(s.solution(nums1, tar1));
+        s.count = 0;
+        System.out.println(s.solution(nums2, tar2));
+    }
+}
